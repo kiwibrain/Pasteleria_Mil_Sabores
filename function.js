6c23c2727135329884ecd530467d98b883f38ba4
kiwibrain <mik.gonzalez@duocuc.cl>
2026-09-01 19:15:48 -0400

    se agrego pagina para producto especifico

--- function.js
+++ function.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.slider');
-var options = {
-    indicators: true,
-    height: 600,
-    duration: 500,
-    interval: 6000
-}
+    var options = {
+        indicators: true,
+        height: 600,
+        duration: 500,
+        interval: 6000
+    }
     var instances = M.Slider.init(elems, options);
 });
 
 instances.start();
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+});
