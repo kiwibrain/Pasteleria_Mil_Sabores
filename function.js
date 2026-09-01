6b3b097a2581b5e66729f1ecb1b96accdfce1f82
kiwibrain <mik.gonzalez@duocuc.cl>
2026-09-01 17:23:05 -0400

    se agrego slides de imagenes a pagina principal

---
+++ function.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.slider');
+var options = {
+    indicators: true,
+    height: 600,
+    duration: 500,
+    interval: 6000
+}
+    var instances = M.Slider.init(elems, options);
+});
+
+instances.start();
